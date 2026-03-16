2bef000a7213474982bb1299def7b53f1614381c
DULANA CHATHURMA <dulanachathurma99@gmail.com>
2026-03-16 11:31:46 +0530

    Add topkfrequent function to find top k elements
    Implement a function to find the top k frequent elements in a list and test it with sample data.

---
+++ tasks/task_01.py
@@ -0,0 +1,34 @@
+def topkfrequent(numbers,k):
+
+    count = {}
+
+    for n in numbers:
+        if n not in count:
+            count[n] = 1
+        else:
+            count[n] = count[n] + 1
+
+    items = []
+
+    for num in count:
+        items.append([num, count[num]])
+
+    items.sort(key=lambda a: (-a[1], a[0]))
+
+    result = []
+
+    for i in range(k):
+        result.append(items[i][0])
+
+    return result
+
+numbers = [1, 1, 1, 2, 2, 3]
+k = 2
+
+print(topkfrequent(numbers,k))
+
+numbers =[4, 4, 4, 6, 6, 1, 1, 1, 1, 2]
+k = 2
+
+print(topkfrequent(numbers,k))
+
