be0e39275980fe9e7ad7b51a3b6b6e1c39a46688
DULANA CHATHURMA <dulanachathurma99@gmail.com>
2026-03-18 22:12:01 +0530

    Update task_01.py

--- tasks/task_01.py
+++ tasks/task_01.py
@@ -1,36 +1,27 @@
-def topkfrequent(numbers,k):
+def topkfrequent(numbers, k):
+    count = {}
+    for n in numbers:
+        if n not in count:
+            count[n] = 1
+        else:
+            count[n] += 1
 
-count = {}
+    items = []
+    for numbers in count:
+        items.append([numbers, count[numbers]])
 
-for n in numbers:
-        
-if n not in count:
- count[n] = 1
-    
-else:
-count[n] = count[n] + 1
+    items.sort(key=lambda a: (-a[1], a[0]))
 
-items = []
+    result = []
+    for i in range(k):
+        result.append(items[i][0])
 
-for numbers in count:
-items.append([numbers, count[numbers]])
-
-items.sort(key=lambda a: (-a[1], a[0]))
-
-result = []
-
-for i in range(k):
-result.append(items[i][0])
-
-return result
+    return result
 
 numbers = [1, 1, 1, 2, 2, 3]
 k = 2
+print(topkfrequent(numbers, k))
 
-print(topkfrequent(numbers,k))
-
-numbers =[4, 4, 4, 6, 6, 1, 1, 1, 1, 2]
+numbers = [4, 4, 4, 6, 6, 1, 1, 1, 1, 2]
 k = 2
-
-print(topkfrequent(numbers,k))
-
+print(topkfrequent(numbers, k))
